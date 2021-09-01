f9e6f02d7d1c73ee85f7ca2c2774fb82970508c8
dearaujo_882564 <stefan.dearaujo64@k12.leanderisd.org>
2021-09-01 12:34:36 -0500

    Added test

--- src/test/java/ProgressBarTest.java
+++ src/test/java/ProgressBarTest.java
@@ -1,10 +1,121 @@
+import net.thejrdev.ProgressBar;
 import org.junit.jupiter.api.Test;
 
-public class ProgressBarTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class ProgressBarTest {
 
     @Test
     public final void constructorTest0(){
 
+        ProgressBar progressBar = new ProgressBar();
+
+        char openChar = progressBar.getOpenChar();
+        char closeChar = progressBar.getCloseChar();
+        char fillChar = progressBar.getFillChar();
+        char emptyChar = progressBar.getEmptyChar();
+
+        char expectedOpenChar = '[';
+        char expectedCloseChar = ']';
+        char expectedFillChar = '|';
+        char expectedEmptyChar = '-';
+
+        assertEquals(expectedCloseChar, closeChar);
+        assertEquals(expectedEmptyChar, emptyChar);
+        assertEquals(expectedOpenChar, openChar);
+        assertEquals(expectedFillChar, fillChar);
+
     }
+    @Test
+    public final void constructorTest1(){
+
+        ProgressBar progressBar = new ProgressBar('|', '-');
+
+        char openChar = progressBar.getOpenChar();
+        char closeChar = progressBar.getCloseChar();
+        char fillChar = progressBar.getFillChar();
+        char emptyChar = progressBar.getEmptyChar();
+
+        char expectedOpenChar = '[';
+        char expectedCloseChar = ']';
+        char expectedFillChar = '|';
+        char expectedEmptyChar = '-';
+
+        assertEquals(expectedCloseChar, closeChar);
+        assertEquals(expectedEmptyChar, emptyChar);
+        assertEquals(expectedOpenChar, openChar);
+        assertEquals(expectedFillChar, fillChar);
+
+    }
+    @Test
+    public final void constructorTest2(){
+
+        ProgressBar progressBar = new ProgressBar('|', '-', (byte) 100);
+
+        char openChar = progressBar.getOpenChar();
+        char closeChar = progressBar.getCloseChar();
+        char fillChar = progressBar.getFillChar();
+        char emptyChar = progressBar.getEmptyChar();
+
+        char expectedOpenChar = '[';
+        char expectedCloseChar = ']';
+        char expectedFillChar = '|';
+        char expectedEmptyChar = '-';
+
+        assertEquals(expectedCloseChar, closeChar);
+        assertEquals(expectedEmptyChar, emptyChar);
+        assertEquals(expectedOpenChar, openChar);
+        assertEquals(expectedFillChar, fillChar);
+
+    }
+    @Test
+    public final void constructorTest3(){
+
+        ProgressBar progressBar = new ProgressBar('[', ']', '|', '-');
+
+        char openChar = progressBar.getOpenChar();
+        char closeChar = progressBar.getCloseChar();
+        char fillChar = progressBar.getFillChar();
+        char emptyChar = progressBar.getEmptyChar();
+
+        char expectedOpenChar = '[';
+        char expectedCloseChar = ']';
+        char expectedFillChar = '|';
+        char expectedEmptyChar = '-';
+
+        assertEquals(expectedCloseChar, closeChar);
+        assertEquals(expectedEmptyChar, emptyChar);
+        assertEquals(expectedOpenChar, openChar);
+        assertEquals(expectedFillChar, fillChar);
+
+    }
+    @Test
+    public final void constructorTest4(){
+
+        ProgressBar progressBar = new ProgressBar('[', ']', '|', '-', (byte)100);
+
+        char openChar = progressBar.getOpenChar();
+        char closeChar = progressBar.getCloseChar();
+        char fillChar = progressBar.getFillChar();
+        char emptyChar = progressBar.getEmptyChar();
+
+        char expectedOpenChar = '[';
+        char expectedCloseChar = ']';
+        char expectedFillChar = '|';
+        char expectedEmptyChar = '-';
+
+        assertEquals(expectedCloseChar, closeChar);
+        assertEquals(expectedEmptyChar, emptyChar);
+        assertEquals(expectedOpenChar, openChar);
+        assertEquals(expectedFillChar, fillChar);
+
+    }
+
+
+
+
+
+
+
+
 }
